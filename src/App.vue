f0022b0bd3cc347b424b981f46f592ff20ed2287
Daria Obrezkova <dariahighfly@gmail.com>
2020-05-17 20:11:20 +0300

    [add] menu [add] styles for all chart components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
     <div id="app">
-        <Navbar/>
-        <div class="box">
-            <StatisticsCharts/>
-            <StatisticsAllTables
-                    :commits="commits"
-                    :changes="changes"
-                    :unstableQueries="unstableQueries"
-                    :runErrors="runErrors"
-                    :skippedTests="skippedTests"
-                    :badTests="badTests"
-                    :testTimes="testTimes"
-                    :slowOnClientTests="slowOnClientTests">
-            </StatisticsAllTables>
+        <Menu></Menu>
+        <div>
+            <Navbar/>
+            <div class="box">
+                <StatisticsCharts/>
+                <StatisticsAllTables
+                        :commits="commits"
+                        :changes="changes"
+                        :unstableQueries="unstableQueries"
+                        :runErrors="runErrors"
+                        :skippedTests="skippedTests"
+                        :badTests="badTests"
+                        :testTimes="testTimes"
+                        :slowOnClientTests="slowOnClientTests">
+                </StatisticsAllTables>
+            </div>
         </div>
     </div>
 </template>
@@ -23,6 +26,7 @@
     import Navbar from "./components/Navbar"
     import StatisticsCharts from "./components/StatisticsCharts"
     import StatisticsAllTables from "./components/StatisticsAllTables"
+    import Menu from "./components/Menu"
 
     export default {
         name: 'app',
@@ -30,6 +34,7 @@
             Navbar,
             StatisticsCharts,
             StatisticsAllTables,
+            Menu
         },
         computed: {
             ...mapGetters({
@@ -54,15 +59,22 @@
 </script>
 
 <style>
+    body {
+        margin: 0;
+    }
     #app {
         font-family: "Yandex Sans Display", sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
     }
     .box {
+        width: 100%;
         padding: 0 20px 0 20px;
+        background-color: #f4f6f8;
+        overflow: scroll;
     }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="menu">
+        <img class="menu__img" src="../../public/logo.svg">
+        <div class="menu__list">
+            <a href="/">Dashboard</a>
+            <a href="/">Tables</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu",
+        data() {
+            return {
+                // isOpen: false
+            }
+        },
+        methods: {
+            // openMenu() {
+            //     return:
+            // }
+        }
+    }
+</script>
+
+<style scoped>
+    .menu {
+        display: flex;
+        flex-direction: column;
+        background-color: #FFFFFF;
+        transition: 0.5s;
+    }
+    .menu__img {
+        height: 50px;
+        width: auto;
+        margin: 20px 0 0 0;
+    }
+    .menu__list {
+        margin: 30px 0 0 0;
+    }
+    .menu__list a {
+        padding: 10px 10px 10px 10px;
+        text-decoration: none;
+        font-size: 16px;
+        color: #818181;
+        display: block;
+        transition: 0.3s;
+    }
+
+    .menu__list a:hover {
+        color: #232323;
+    }
+</style>
